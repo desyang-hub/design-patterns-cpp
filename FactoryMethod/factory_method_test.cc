cd994a2654dedfffde6bf18683d3a812105f0ee8
desyang <desyang@qq.com>
2026-03-27 11:54:46 +0800

    修改工厂方法

--- FactoryMethod/factory_method_test.cc
+++ FactoryMethod/factory_method_test.cc
@@ -1,10 +1,35 @@
 #include "phone_factory.h"
 
+// 模板类的偏特化
+template<class T>
+struct factory_builder {
+    static T Create() {
+        return T();
+    }
+};
+
+template<class T>
+struct factory_builder<T*> {
+    static T* Create() {
+        return new T();
+    }
+};
+
+template<class FactoryImpls>
+auto build_factory() -> decltype(factory_builder<FactoryImpls>::Create()) {
+    return factory_builder<FactoryImpls>::Create();
+}
+
+// 模拟实际场景中的接口
+void take_order(std::unique_ptr<Factory> factory) {
+    factory->Production()->Show();
+}
+
 int main() {
-    // 当有新增需求，或者需求发生变更时，直接新增一个工厂类进行扩展即可
-    auto factory = PhoneFactory();
-    auto product = factory.Production();
-    product->Show();
+    // 以下模拟业务代码，当产品发生改变，无需修改原有接口，秩序修改工厂创建的地方就可以实现业务功能的增加
+    std::unique_ptr<Factory> factory = std::make_unique<PhoneFactory>();
+
+    take_order(std::move(factory));
 
     return 0;
 }
